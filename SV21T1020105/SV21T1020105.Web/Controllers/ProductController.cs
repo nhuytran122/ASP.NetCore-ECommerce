74c0ef0c8cf7866a6c93c16bc0471f4f67350038
nhuytran122 <nhuytran12223@gmail.com>
2024-09-27 15:00:41 +0700

    base prototype

--- SV21T1020105/SV21T1020105.Web/Controllers/ProductController.cs
+++ SV21T1020105/SV21T1020105.Web/Controllers/ProductController.cs
@@ -25,5 +25,42 @@ namespace SV21T1020105.Web.Controllers
         {
             return View();
         }
+
+        public IActionResult Photo(int id = 0, string method = "", int photoId = 0)
+        {
+            switch (method)
+            {
+                case "add":
+                    ViewBag.Title = "Bổ sung ảnh cho mặt hàng";
+                    return View();
+                case "edit":
+                    ViewBag.Title = "Thay đổi ảnh của mặt hàng";
+                    return View();
+                case "delete":
+                    //TODO: Xóa ảnh (xóa trực tiếp, không cần confirm)
+                    return RedirectToAction("Edit", new { id = id });
+                default:
+                    return RedirectToAction("Index");
+            }
+        }
+
+        public IActionResult Attribute(int id = 0, string method = "", int attributeId = 0)
+        {
+            switch (method)
+            {
+                case "add":
+                    ViewBag.Title = "Bổ sung thuộc tính cho mặt hàng";
+                    return View();
+                case "edit":
+                    ViewBag.Title = "Thay đổi thuộc tính của mặt hàng";
+                    return View();
+                case "delete":
+                    //TODO: Xóa thuộc tính (xóa trực tiếp, không cần confirm)
+                    return RedirectToAction("Edit", new { id = id });
+                default:
+                    return RedirectToAction("Index");
+            }
+        }
+
     }
 }
