b2a379305649907a49365fe7a79a5801bc8e7cae
nhuytran122 <nhuytran12223@gmail.com>
2024-12-13 10:09:00 +0700

    update show all Product (!categoryName)

---
+++ SV21T1020105/SV21T1020105.Shop/Controllers/ProductController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SV21T1020105.BusinessLayers;
+using SV21T1020105.Shop.Models;
+
+namespace SV21T1020105.Shop.Controllers
+{
+    public class ProductController : Controller
+    {
+        public const int PAGE_SIZE = 9;
+        private const string PRODUCT_SEARCH_CONDITION = "ProductSearchCondition";
+        public IActionResult Index()
+        {
+            ProductSearchInput? condition = ApplicationContext.GetSessionData<ProductSearchInput>(PRODUCT_SEARCH_CONDITION);
+            if (condition == null)
+                condition = new ProductSearchInput()
+                {
+                    Page = 1,
+                    PageSize = PAGE_SIZE,
+                    SearchValue = "",
+                    CategoryID = 0,
+                    SupplierID = 0,
+                    MinPrice = 0,
+                    MaxPrice = 0,
+                };
+            return View(condition);
+        }
+
+        public IActionResult Search(ProductSearchInput condition)
+        {
+            int rowCount;
+            var data = ProductDataService.ListProducts(out rowCount, condition.Page, condition.PageSize, condition.SearchValue ?? "", condition.CategoryID, condition.SupplierID, condition.MinPrice, condition.MaxPrice);
+            ProductSearchResult model = new ProductSearchResult()
+            {
+                Page = condition.Page,
+                PageSize = condition.PageSize,
+                SearchValue = condition.SearchValue ?? "",
+                RowCount = rowCount,
+                CategoryID = condition.CategoryID,
+                SupplierID = condition.SupplierID,
+                MinPrice = condition.MinPrice,
+                MaxPrice = condition.MaxPrice,
+                Data = data
+            };
+
+            ApplicationContext.SetSessionData(PRODUCT_SEARCH_CONDITION, condition);
+
+            return View(model);
+        }
+    }
+}
---
+++ SV21T1020105/SV21T1020105.Shop/Models/PaginationSearchResult.cs
@@ -0,0 +1,34 @@
+﻿using SV21T1020105.DomainModels;
+
+namespace SV21T1020105.Shop.Models
+{
+    public abstract class PaginationSearchResult
+    {
+        public int Page { get; set; }
+        public int PageSize { get; set; }
+        public string SearchValue { get; set; } = "";
+        public int RowCount { get; set; }
+        public int PageCount
+        {
+            get
+            {
+                if (PageSize == 0)
+                    return 1;
+                int c = RowCount / PageSize;
+                if (RowCount % PageSize > 0)
+                    c += 1;
+
+                return c;
+            }
+        }
+    }
+    public class ProductSearchResult : PaginationSearchResult
+    {
+        public int CategoryID { get; set; } = 0;
+        public int SupplierID { get; set; } = 0;
+        public decimal MinPrice { get; set; } = 0;
+        public decimal MaxPrice { get; set; } = 0;
+        public required List<Product> Data { get; set; }
+    }
+}
+
